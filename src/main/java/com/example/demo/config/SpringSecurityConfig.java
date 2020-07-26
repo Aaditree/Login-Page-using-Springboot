a4ed4eecdfa5113cffcfee64996f84b9aaa9c4a6
sunil <sunil@idea-PC>
2020-07-27 00:59:34 +0530

    Aaditree commmitted login page

---
+++ src/main/java/com/example/demo/config/SpringSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("Aaditree")
+
+.password("QWERTY").roles("ADMIN");	
+		auth.inMemoryAuthentication().withUser("MANAGER").password("Password2").roles("USER");	}
+
+// security based on ROLE	
+	@Override	protected void configure(HttpSecurity http) throws Exception {	
+		http.csrf().disable();	
+		http.authorizeRequests().antMatchers("/login/**").hasAnyRole("ADMIN").anyRequest().fullyAuthenticated().and().httpBasic();	
+	}
+	
+	
+	@Bean	public static NoOpPasswordEncoder passwordEncoder() 
+	{		
+		return (NoOpPasswordEncoder) 
+				NoOpPasswordEncoder.getInstance();
+		}}
